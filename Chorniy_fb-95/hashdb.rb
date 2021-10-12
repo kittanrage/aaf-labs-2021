f10d8c1ce30cc79aaf7929c0624ec8acb85437c9
kittenrage <anacho-ipt23@lll.kpi.ua>
2021-10-12 16:07:56 +0300

    lexical analysis 0.4

--- Chorniy_fb-95/hashdb.rb
+++ Chorniy_fb-95/hashdb.rb
@@ -1,37 +1,6 @@
 require_relative "modules.rb"
 include T1
 
-class DB
-    attr_accessor :col, :row
-    
-    def initialize(hash)
-      self.col = hash[:col]
-      self.row = hash[:row]
-    end
-    
-    def Is4
-        if self.col == 4
-            return true
-        end
-        return false
-    end
-    
-    def insert()
-        puts "inserted"
-        return true
-    end
-    
-    def delete()
-        puts "deleted"
-        return true
-    end
-    
-    def select()
-        puts "selected"
-    end
-
-end
-
 cmd = "start"
 while (!cmd.include? "exit;")
 
@@ -39,6 +8,10 @@ while (!cmd.include? "exit;")
     #system "cls"
     puts"\ndb is open"
     cmd = gets(';').gsub("\n",' ').gsub(/\s+/, ' ').chomp
+    if !cmd.match(/\b(create)\b|\b(insert)\b|\b(select)\b|\b(delete)\b|\b(exit)\b/)
+        cmd = '' 
+        puts 'there are no commands in your query'
+    end
     print("\n")
     
     T1.create(cmd) if (cmd.match(/create/i))  
--- Chorniy_fb-95/modules.rb
+++ Chorniy_fb-95/modules.rb
@@ -1,43 +1,123 @@
+BUCKETS = 401
+
+class Tables
+
+    attr_accessor :table, :col, :row, :key, :value
+    @@all = []
+    
+    def initialize(table, col, row, value)
+        @table = table.to_s
+        @col = col.to_s
+        @row = row
+        @value = value.to_s
+        @key = @table.codepoints.join.to_i * @value.codepoints.join.to_i * @col.codepoints.join.to_i % BUCKETS
+        @@all << self
+    end
+
+    def value=(newvalue)
+        @value = newvalue
+    end
+
+    def row=(newrow)
+        @row = newrow
+    end
+
+    def self.all
+        @@all
+    end
+
+    def table
+        @table
+    end
+
+    def col
+        @col
+    end
+
+    def row
+        @row
+    end
+
+    def key
+        @key
+    end
+
+    def value
+        @value
+    end
+    
+    def self.print_all#(name)
+        @@all.each do |col| 
+            p col.key #if col.value == name
+        end
+    end
+
+    def self.find_by(tbl, value, col)
+        key = tbl.codepoints.join.to_i * value.codepoints.join.to_i * col.codepoints.join.to_i % BUCKETS
+        tmp = @@all.find{|col| col.key == key}
+        @@all.each do |col| 
+            p col.value if col.row == tmp.row 
+        end
+    end
+
+end
+
 module T1
+
+    #@rows = {}
 
     def create(cmd)
 
-        if cmd.match(/\(\)/) || !cmd.match(/\(/) || !cmd.match(/\)/) || !cmd.match(/^.+?(?=\()/).to_s.match(/create\s\w+/i)
+        if cmd.match(/\(\)/) || !cmd.match(/\(/) || !cmd.match(/\)/) || !cmd.match(/(create)(?=\s\w+\s\()/)
             puts "there are no arguments! \ntry to use brackets in a correct way"
         else
             tbl = cmd.match(/(?<=create\s)\w+/i).to_s
             cols = cmd.scan(/(?<=\()(.*?)(?=\,)|(?<=\,)(.*?)(?=\,)|(?<=\,)(.*?)(?=\))/i) - [nil, '']
-            ic = Array.new
+            #@rows[tbl] = [cols, 0]
+            p cols
+            ic = []
             for col in 0..cols.length() - 1
                 cols[col] -= [nil]
-                cols[col] = cols[col][0].to_s.split(/[\s\,]/) - [nil, '']
+                cols[col] = cols[col][0].to_s.gsub(/^\s/, '').split(/(\s(?=indexed))/) - [nil, '', ' ']
                 ic[col] = cols[col][0] if cols[col][1].to_s.match(/indexed/i)
             end
+            ic -= [nil]
+            p cols
+            #p Tables.all
             puts"there are #{cols.length.to_s} columns"
             puts"indexed columns are: #{ic}"
         end
-    
+        #p @rows
     end
     
     def insert(cmd)
     
-        if cmd.match(/\(\)/) || !cmd.match(/\(/) || !cmd.match(/\)/) || !cmd.match(/^.+?(?=\()/).to_s.match(/insert\s\w+/)
+        if cmd.match(/\(\)/) || !cmd.match(/\(/) || !cmd.match(/\)/) || !cmd.match(/(insert)(?=\s\w+\s\()|(insert)(?=\s\w+\s\w+\s\()/)
             puts"there are no arguments! \ntry to use brackets"
         else
             tbl = cmd.match(/(?<=insert\s)\w+/i).to_s
             tbl = cmd.match(/(?<=insert\sinto\s)\w+/i).to_s if tbl.match(/into/i)
+            #@rows[tbl][1] += 1
             puts tbl
-            values = cmd.gsub(/^(.*?)\(/, '').split(/[\s\,\"\)\;]/) - [nil, '']
-            puts values
+            values = cmd.gsub(/^(.*?)\(|^\s/, '').split(/[\,\;\)\()]/) - [nil, '', ' ']
+            #p values
+            # i = 0
+            # @rows[tbl][0].each do |n| 
+            #     n = Tables.new(tbl, n.to_s.match(/\w+/), @rows[tbl][1], values[i])
+            #     i += 1
+            # end
+            #p Tables.all
+            p values
             puts"1 row with #{values.length.to_s} values added to table #{tbl.to_s}"
         end
-    
+        #puts @rows
     end
     
     def delete(cmd)
     
         tbl = cmd.match(/(?<=delete\s)\w+/i).to_s
         tbl = cmd.match(/(?<=delete\sfrom\s)\w+/i).to_s if tbl.match(/from/i)
+        #@rows[tbl] -= 1
         puts tbl
         if !tbl.match(/\w+/)
             puts"error occured"
@@ -52,6 +132,7 @@ module T1
                     puts cnd
                 end
             end
+            #puts @rows
         end
     
     end
@@ -60,14 +141,14 @@ module T1
     
         tbl = cmd.match(/(?<=from\s)\w+/i).to_s
         if (cmd.match(/(?<=select\s)\*(?=\sfrom)/i))
-            puts"entire table " + tbl + " selected"
+            puts"all columns in table " + tbl + " selected"
         else
             if !cmd.match(/where/i)
                 puts"table #{tbl} selected"
             else
                 if cmd.match(/\(\)/) || !cmd.match(/\(/) || !cmd.match(/\)/) || !cmd.match(/^.+?(?=\()/).to_s.match(/select.+from.+where/)
                     puts"there are no arguments! \ntry to use brackets"
-                else
+                
                     cols = cmd.match(/(?<=select\s)(.*?)(?=\sfrom)/i).to_s.split(/[\s\,]/) - [nil, '']
                     puts cols
                     cnd = cmd.scan(/\((.*?)\)/)
@@ -75,7 +156,9 @@ module T1
                 end
             end
         end
-    
+        #Tables.print_all
+        #Tables.find_by(tbl, 'green', 'color')
+        #puts @rows    
     end
 
 end
